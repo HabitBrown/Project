ba8521b1c16c9cb7ef247fcc22deddd935d11e62
Lovepple <noble2534@naver.com>
2025-09-28 21:07:58 +0900

    [fix] : SQL문 ORM 방식으로 수정

--- backend/app/routers/register.py
+++ backend/app/routers/register.py
@@ -1,49 +1,76 @@
 import hashlib
-import connect_sql as cs
 import datetime
 from typing import Optional
+from sqlalchemy.exc import IntegrityError
+from backend.app.database import SessionLocal
+from backend.app.models.user import User
 
 class Register(object):
     # 회원가입 시 사용하는 유저의 정보
-    def __init__(self, phone: str, password: str, nickname: str, gender: Optional[str] = None, age: Optional[int] = None):
-        self.__phone = phone
-        self.__password = hashlib.sha256((password + phone).encode()).hexdigest()
-        self.__nickname = nickname
-        self.__gender = gender
-        self.__age = age
-        self.__bio = 'none'
-        self.__profile_picture = 'none'
+    def __init__(self, phone: str, password: str, nickname: str, gender: Optional[str] = None, age: Optional[int] = None, timezone: Optional[str] = None):
+        self._phone = phone
+        self._password = hashlib.sha256((password + phone).encode()).hexdigest()
+        self._nickname = nickname
+        self._gender = gender
+        self._age = age
+        self._bio = 'none'
+        self._profile_picture = 'none'
 
         # 시작 금액은 0이므로 (가변가능)
-        self.__hb_balance = 0
+        self._hb_balance = 0
 
         # created_at은 어디를 기준으로 할 지 논의가 필요
-        self.__created_at = datetime.datetime.now()
+        self._created_at = datetime.datetime.now()
+        self._timezone = timezone or "Asia/Seoul"
 
     def register_user(self):
-        con = cs.connect_sql("", "", "", "")
-        cur = con.cursor()
+        user = User(
+            phone=self._phone,
+            password_hash=self._password,
+            nickname=self._nickname,
+            gender=self._gender,
+            age=self._age,
+            bio=self._bio,
+            profile_picture=self._profile_picture,
+            timezone=self._timezone,
+            hb_balance=self._hb_balance,
+            created_at=self._created_at,
+        )
 
-        try:
-            # 만약 신규 가입이라면 INSERT를 진행하고,
-            query = f"""INSERT INTO 
-            User(phone, password_hash, nickname, gender, age, bio, 
-            profile_picture, hb_balance, created_at) 
-            VALUES({self.__phone}, {self.__password}, {self.__nickname}, {self.__gender},
-            {self.__age}, {self.__bio}, {self.__profile_picture}, {self.__hb_balance}, {self.__created_at})"""
+        with SessionLocal() as db:
+            db.add(user)
+            try:
+                db.commit()
+            except IntegrityError as e:
+                db.rollback()
+                raise ValueError("에러") from e
+            db.refresh(user)
+            return user
 
-        except:
-            # uid를 기준으로 찾아서 수정, 재화/생성일은 수정할 필요가 없으므로 넣지 않음.
-            temp = "유저 아이디"
-            query = f"""UPDATE User SET
-            phone = {self.__phone},
-            password_hash = {self.__password}, 
-            nickname = {self.__nickname},
-            gender = {self.__gender},
-            age = {self.__age}, 
-            bio = {self.__bio}, 
-            profile_picture = {self.__profile_picture}  
-            WHERE id = {temp}"""
+
+        # con = cs.connect_sql("", "", "", "")
+        # cur = con.cursor()
+
+        # try:
+        #     # 만약 신규 가입이라면 INSERT를 진행하고,
+        #     query = f"""INSERT INTO
+        #     User(phone, password_hash, nickname, gender, age, bio,
+        #     profile_picture, hb_balance, created_at)
+        #     VALUES({self._phone}, {self._password}, {self._nickname}, {self._gender},
+        #     {self._age}, {self._bio}, {self._profile_picture}, {self._hb_balance}, {self._created_at})"""
+        #
+        # except:
+        #     # uid를 기준으로 찾아서 수정, 재화/생성일은 수정할 필요가 없으므로 넣지 않음.
+        #     temp = "유저 아이디"
+        #     query = f"""UPDATE User SET
+        #     phone = {self._phone},
+        #     password_hash = {self._password},
+        #     nickname = {self._nickname},
+        #     gender = {self._gender},
+        #     age = {self._age},
+        #     bio = {self._bio},
+        #     profile_picture = {self._profile_picture}
+        #     WHERE id = {temp}"""
 
         cur.execute(query)
         con.commit()
